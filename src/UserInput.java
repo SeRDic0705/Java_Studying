0f0cd19dc7f75faf9e6245a9a9593600164e53cd
SeRDic0705 <SeRDic0705@github.com>
2019-04-12 19:02:33 +0900

    Update UserInput.java
    Add "Exception Handling"

--- src/UserInput.java
+++ src/UserInput.java
@@ -5,10 +5,30 @@ public class UserInput {
 	private static Scanner scan = new Scanner(System.in);
 	
 	public static double getDouble() {
-		return Double.parseDouble(scan.nextLine());
+		double input = 0;
+		while(true) {
+			try {
+				input = Double.parseDouble(scan.nextLine());
+				break;
+			}
+			catch(NumberFormatException e) {
+				System.out.println("올바른 값(실수)를 입력해주세요.");
+			}
+		}
+		return input;
 	}
 	public static int getInteger() {
-		return Integer.parseInt(scan.nextLine());
+		int input = 0;
+		while(true) {
+			try {
+				input = Integer.parseInt(scan.nextLine());
+				break;
+			}
+			catch(NumberFormatException e) {
+				System.out.println("올바른 값(정수)를 입력해주세요.");
+			}
+		}
+		return input;
 	}
 	public static String getString() {
 		return scan.nextLine();
